ca87c12dc6035e422f298192100d4f5193c32b51
Mohammad Imteyaz <52629525+imteyazm745@users.noreply.github.com>
2022-10-30 14:54:37 +0530

    Create ar_operation.sh

---
+++ Hackerrank/ar_operation.sh
@@ -0,0 +1,10 @@
+# Given two integers, X and Y , find their sum, difference, product, and quotient
+# Four lines containing the sum (X+Y), difference (X-Y), product (X*Y), and quotient (X/Y), respectively.
+# (While computing the quotient, print only the integer part.)
+
+read X
+read Y
+echo $((X+Y))
+echo $((X-Y))
+echo $((X*Y))
+echo $((X/Y))
