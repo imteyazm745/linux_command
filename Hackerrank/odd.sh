888955756eacc8e2ef34a968a240c39d1ebfb459
Mohammad Imteyaz <52629525+imteyazm745@users.noreply.github.com>
2022-10-30 14:51:04 +0530

    Update odd.sh

--- Hackerrank/odd.sh
+++ Hackerrank/odd.sh
@@ -1,3 +1,4 @@
+#Looping and skipping path
 #Your task is to use for loops to display only odd natural numbers from  1 to 99.
 for i in {1..100..2} #starting number 1 end number 99 and incremented by 2 
 do
