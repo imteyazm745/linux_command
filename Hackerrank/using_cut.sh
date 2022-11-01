93e0d75c7f9458c3185a7541e8f2287d2bb154e8
Mohammad Imteyaz <52629525+imteyazm745@users.noreply.github.com>
2022-11-01 20:46:54 +0530

    Update using_cut.sh

--- Hackerrank/using_cut.sh
+++ Hackerrank/using_cut.sh
@@ -1,8 +1,21 @@
-#Given N lines of input, print the 3rd character from each line as a new line of output.
+#1. Given N lines of input, print the 3rd character from each line as a new line of output.
 #It is guaranteed that each of the n lines of input will have a 3rd character.
 
 #solution :- 
 cut -c 3
 
-#Display the 2nd and 7th character from each line of text.
+#2. Display the 2nd and 7th character from each line of text.
+
+#Sample Input- Hello,World,how are you
+
+#Sample Output -e,o,oe
+
 cut -c 2,7 #it will print 2nd and the 7th character from each line of text.
+
+#3. Display a range of characters starting at the 2nd position of a string and ending at the 7th position (both positions included).
+#Sample Input - Hello, World, how are you
+
+#Sample Output - ello, orld, ow are
+
+#Solution
+cut -c 2-7
